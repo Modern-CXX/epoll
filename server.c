d22dae1603b1323b1aa35a7c4099c74787150449
ljhm <9011764+ljhm@users.noreply.github.com>
2022-08-28 01:44:18 +0800

    Update server.c

--- server.c
+++ server.c
@@ -12,31 +12,50 @@
 #include <errno.h>
 #include <signal.h>
 
+#define PRINT_LOG(fmt, ...) do \
+{ \
+    printf("%s:%d:%s: " fmt "\n", \
+        __FILE__, __LINE__, __func__, ##__VA_ARGS__); \
+} while (0) /* ; no trailing semicolon here */
+
+#define PERROR(fmt, ...) do \
+{ \
+    printf("%s:%d:%s: %s. " fmt "\n", \
+        __FILE__, __LINE__, __func__, strerror(errno), ##__VA_ARGS__); \
+} while (0) /* ; no trailing semicolon here */
+
 void set_nonblock(int fd)
 {
     int flags = fcntl(fd, F_GETFL);
     if (flags == -1) {
-        perror("fcntl");
-        exit(-1);
+        PERROR("fcntl");
+        exit(EXIT_FAILURE);
     }
 
     flags |= O_NONBLOCK;
     if (fcntl(fd, F_SETFL, flags) == -1) {
-        perror("fcntl");
-        exit(-1);
+        PERROR("fcntl");
+        exit(EXIT_FAILURE);
     }
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     struct epoll_event ev, events[10];
     int listenfd, connfd, nfds, efd;
 
     /* set up listening socket */
     int port = 8000;
-    int ret = 0, count = 0;
+    int ret = 0, conn_count = 0;
     struct sockaddr_in my_addr, peer_addr;
     socklen_t addrlen = sizeof(peer_addr);
+
+    if (argc == 2){
+        int n = atoi(argv[1]);
+        if (n != 0) {
+            port = n;
+        }
+    }
 
     memset(&my_addr, 0, sizeof(my_addr));
     my_addr.sin_port = htons(port);
@@ -45,20 +64,20 @@ int main()
 
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
     if (listenfd == -1) {
-        perror("socket");
+        PERROR("socket");
         exit(EXIT_FAILURE);
     }
 
     ret = bind(listenfd, (struct sockaddr*)&my_addr, sizeof(my_addr));
     if (ret == -1) {
-        perror("bind");
+        PERROR("bind");
         close(listenfd);
         exit(EXIT_FAILURE);
     }
 
     ret = listen(listenfd, SOMAXCONN);
     if (ret == -1) {
-        perror("listen");
+        PERROR("listen");
         close(listenfd);
         exit(EXIT_FAILURE);
     }
@@ -68,66 +87,106 @@ int main()
     /* set up epoll */
     efd = epoll_create1(0);
     if (efd == -1) {
-        perror("epoll_create1");
+        PERROR("epoll_create1");
         exit(EXIT_FAILURE);
     }
+
     ev.events = EPOLLIN;
     ev.data.fd = listenfd;
     if (epoll_ctl(efd, EPOLL_CTL_ADD, listenfd, &ev) == -1) {
-        perror("epoll_ctl listenfd");
+        PERROR("epoll_ctl");
         exit(EXIT_FAILURE);
     }
+
     for (;;) {
         nfds = epoll_wait(efd, events, sizeof(events) / sizeof(events[0]), 1);
         if (nfds == -1) {
-            perror("epoll_wait");
+            PERROR("epoll_wait");
             exit(EXIT_FAILURE);
         }
 
-        for (int n = 0; n < nfds; ++n) {
-            if (events[n].data.fd == listenfd) {
+        for (int i = 0; i < nfds; i++) {
+            if (events[i].data.fd == listenfd) {
                 connfd = accept(listenfd,
                                 (struct sockaddr *) &peer_addr, &addrlen);
                 if (connfd == -1) {
-                    perror("accept");
+                    PERROR("accept");
                     exit(EXIT_FAILURE);
                 }
                 set_nonblock(connfd);
-                count++; //
-                printf("accept connection from client %s:%d, count: %d\n",
-                    inet_ntoa(peer_addr.sin_addr), peer_addr.sin_port, count);
+
+                conn_count++; //
+                PRINT_LOG("accept connection from client %s:%u, conn_count: %d",
+                    inet_ntoa(peer_addr.sin_addr), peer_addr.sin_port,
+                    conn_count);
 
                 ev.events = EPOLLIN | EPOLLET;
                 ev.data.fd = connfd;
                 if (epoll_ctl(efd, EPOLL_CTL_ADD, connfd, &ev) == -1) {
-                    perror("epoll_ctl connfd");
+                    PERROR("epoll_ctl");
                     exit(EXIT_FAILURE);
                 }
 
-            } else if (events[n].events & EPOLLIN) {
-                //errno == EAGAIN || errno == EWOULDBLOCK
-                char buf[1024] = {0};
-                ret = read(events[n].data.fd, buf, sizeof(buf) - 1);
+            } else if (events[i].events & EPOLLIN) {
+                int fd = events[i].data.fd;
+                int done = 0;
+                for (;;){
+                    char buf[1024] = {0};
+                    ret = read(fd, buf, sizeof(buf) - 1);
+                    int err = errno;
 
-                if (strlen(buf) > 0)
-                    printf("%s\n", buf);
+                    if (ret == -1) {
+                        break;
+                    }
+                    if (ret == 0){
+                        close(fd);
+                        done = 1;
+                        break;
+                    }
+                    PRINT_LOG("%s", buf);
+                }
+                if (done){
+                    break;
+                }
 
                 ev.events = EPOLLOUT | EPOLLET;
-                ev.data.fd = events[n].data.fd;
-                epoll_ctl(efd, EPOLL_CTL_MOD, events[n].data.fd, &ev);
+                ev.data.fd = fd;
+                if (epoll_ctl(efd, EPOLL_CTL_MOD, fd, &ev) == -1) {
+                    PERROR("epoll_ctl");
+                    exit(EXIT_FAILURE);
+                }
 
-            } else if (events[n].events & EPOLLOUT){
-                //errno == EAGAIN || errno == EWOULDBLOCK
-                sleep(1); //test
-                const char *msg = "hello client ";
-                ret = write(events[n].data.fd, msg, strlen(msg));
+            } else if (events[i].events & EPOLLOUT){
+                int fd = events[i].data.fd;
+                int done = 0;
+                for (;;){
+                    const char *msg = "hello client ";
+                    sleep(1); //test
+                    ret = write(fd, msg, strlen(msg));
+                    if (ret == -1 || errno == EAGAIN || errno == EPIPE) {
+                        break;
+                    }
+                    if (ret == 0) {
+                        close(fd);
+                        done = 1;
+                        break;
+                    }
+                }
+                if (done){
+                    break;
+                }
 
                 ev.events = EPOLLIN | EPOLLET;
-                ev.data.fd = events[n].data.fd;
-                epoll_ctl(efd, EPOLL_CTL_MOD, events[n].data.fd, &ev);
+                ev.data.fd = fd;
+                if (epoll_ctl(efd, EPOLL_CTL_MOD, fd, &ev) == -1) {
+                    PERROR("epoll_ctl");
+                    exit(EXIT_FAILURE);
+                }
 
             } else {
-                printf("unknown event");
+                PRINT_LOG("unknown event");
+                int fd = events[i].data.fd;
+                close(fd);
                 exit(EXIT_FAILURE);
 
             }
